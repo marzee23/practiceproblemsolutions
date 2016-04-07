17b6624e0977f0f5ff2f739dd0a1797cab968341
marzee23 <marcie.longo@gmail.com>
2016-04-07 13:21:19 -0400

    Update Count_Vowels.py

--- Count_Vowels.py
+++ Count_Vowels.py
@@ -1,11 +1,8 @@
 #Count Vowels - Enter a string and the program counts the number of vowels in the text. 
 #For added complexity have it report a sum of each vowel found
 
-#list function cuts user-inputted string into letters
-StringToCount = list(input("Enter a phrase to count the vowels: "))
-
-#make lowercase using list comprehension
-[letter.lower() for letter in StringToCount]
+#list function cuts user-inputted string into letters and make lowercase
+StringToCount = list(input("Enter a phrase to count the vowels: ").lower())
 
 #sets can only contain unique values
 vowels = {"a", "e", "i", "o", "u"}
@@ -16,4 +13,4 @@ for letter in StringToCount:
 	if letter in vowels:
 		VowelsInString.append(letter)
 
-print(len(VowelsInString))
+print("There are",len(VowelsInString), "vowels in that text!")
