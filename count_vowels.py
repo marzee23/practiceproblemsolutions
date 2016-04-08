24014a5fcdc2c598d03cce09cf56193d8afbd282
Domenic Ariaudo <d.ariaudo@dickblick.com>
2016-04-08 15:01:43 -0400

    Added tests

--- count_vowels.py
+++ count_vowels.py
@@ -1,11 +1,13 @@
 #Count Vowels - Enter a string and the program counts the number of vowels in the text. 
 #For added complexity have it report a sum of each vowel found
 import sys
+import unittest
+
+vowels = {"a", "e", "i", "o", "u"}
 
 def count_each_vowel(in_string):
-    vowels = {"a", "e", "i", "o", "u"}
     vowel_counts = dict()
-    for letter in arg_string:
+    for letter in in_string.lower():
         if letter in vowels:
             new_count = vowel_counts.get(letter, 0) + 1
             vowel_counts[letter] = new_count
@@ -14,10 +16,25 @@ def count_each_vowel(in_string):
 def tabulate_vowel_count(vowel_counts):
     return sum(vowel_counts.values())
 
+class TestCountVowels(unittest.TestCase):
+    def vowel_dict(self, *args):
+        sorted_vowels = ['a','e','i','o','u']
+        return {v:c for v,c in zip(sorted_vowels, args) if c > 0}
+
+    def check_count_each_vowel(self, test_string, expected_output):
+        self.assertEqual(count_each_vowel(test_string), self.vowel_dict(*expected_output))
+
+    def test_count_each_vowel_01(self):
+        self.check_count_each_vowel('aeiou', [1,1,1,1,1])
+
+    def test_count_each_vowel_02(self):
+        self.check_count_each_vowel('What is your name?', [2,1,1,1,1])
+
+    def test_count_each_vowel_03(self):
+        self.check_count_each_vowel('AeIoU', [1,1,1,1,1])
+
+    def test_count_each_vowel_04(self):
+        self.check_count_each_vowel('AeoU', [1,1,0,1,1])
+
 if __name__ == '__main__':
-    arg_string = " ".join(sys.argv[1:])
-    vowel_counts = count_each_vowel(arg_string)
-    total_vowels = tabulate_vowel_count(vowel_counts)
-    print("There are", total_vowels, "vowels in '{}'".format(arg_string))
-    for vowel in sorted(vowel_counts.keys()):
-        print("There are", vowel_counts.get(vowel, 0), vowel.upper(), "'s!")
+    unittest.main()
