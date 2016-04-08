75628e2e6a547239f0ec0a5299d643577020d5f4
Domenic Ariaudo <d.ariaudo@dickblick.com>
2016-04-08 08:30:54 -0400

    Resolved merge conflict with master

--- Count_Vowels.py
+++ Count_Vowels.py
@@ -1,23 +1,20 @@
 #Count Vowels - Enter a string and the program counts the number of vowels in the text. 
 #For added complexity have it report a sum of each vowel found
-import sys
 
-def count_each_vowel(in_string):
-    vowels = {"a", "e", "i", "o", "u"}
-    vowel_counts = dict()
-    for letter in arg_string:
-        if letter in vowels:
-            new_count = vowel_counts.get(letter, 0) + 1
-            vowel_counts[letter] = new_count
-    return vowel_counts
+#list function cuts user-inputted string into letters and make lowercase
+StringToCount = list(input("Enter a phrase to count the vowels: ").lower())
 
-def tabulate_vowel_count(vowel_counts):
-    return sum(vowel_counts.values())
+#sets can only contain unique values
+vowels = {"a", "e", "i", "o", "u"}
 
-if __name__ == '__main__':
-    arg_string = " ".join(sys.argv[1:])
-    vowel_counts = count_each_vowel(arg_string)
-    total_vowels = tabulate_vowel_count(vowel_counts)
-    print("There are", total_vowels, "vowels in '{}'".format(arg_string))
-    for vowel in sorted(vowel_counts.keys()):
-        print("There are", vowel_counts.get(vowel, 0), vowel.upper(), "'s!")
+VowelsInString = []
+	
+for letter in StringToCount:
+	if letter in vowels:
+		VowelsInString.append(letter)
+
+print("There are",len(VowelsInString), "vowels in that text!")
+
+#Counts each letter
+for vowel in sorted(list(vowels)):
+	print("There are", VowelsInString.count(vowel), vowel.upper(), "'s!")
