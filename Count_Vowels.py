238487933001ff55e69c1a302c81b76d732d8ed2
Domenic Ariaudo <d.ariaudo@dickblick.com>
2016-04-08 08:43:22 -0400

    Added my solution for count_vowels to this branch

--- Count_Vowels.py
+++ Count_Vowels.py
@@ -1,20 +1,23 @@
 #Count Vowels - Enter a string and the program counts the number of vowels in the text. 
 #For added complexity have it report a sum of each vowel found
+import sys
 
-#list function cuts user-inputted string into letters and make lowercase
-StringToCount = list(input("Enter a phrase to count the vowels: ").lower())
+def count_each_vowel(in_string):
+    vowels = {"a", "e", "i", "o", "u"}
+    vowel_counts = dict()
+    for letter in arg_string:
+        if letter in vowels:
+            new_count = vowel_counts.get(letter, 0) + 1
+            vowel_counts[letter] = new_count
+    return vowel_counts
 
-#sets can only contain unique values
-vowels = {"a", "e", "i", "o", "u"}
+def tabulate_vowel_count(vowel_counts):
+    return sum(vowel_counts.values())
 
-VowelsInString = []
-	
-for letter in StringToCount:
-	if letter in vowels:
-		VowelsInString.append(letter)
-
-print("There are",len(VowelsInString), "vowels in that text!")
-
-#Counts each letter
-for vowel in sorted(list(vowels)):
-	print("There are", VowelsInString.count(vowel), vowel.upper(), "'s!")
+if __name__ == '__main__':
+    arg_string = " ".join(sys.argv[1:])
+    vowel_counts = count_each_vowel(arg_string)
+    total_vowels = tabulate_vowel_count(vowel_counts)
+    print("There are", total_vowels, "vowels in '{}'".format(arg_string))
+    for vowel in sorted(vowel_counts.keys()):
+        print("There are", vowel_counts.get(vowel, 0), vowel.upper(), "'s!")
